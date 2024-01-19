caee822b3d8dd0ecaa09259bb14e2a4228be2797
Liubov2909 <ostapcuk2909@gmail.com>
2024-01-19 22:46:08 +0200

    dz 12 js i hope so

--- src/main.js
+++ src/main.js
@@ -6,17 +6,20 @@ import axios from 'axios';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
-const loaderTopBtn = document.querySelector('.loader-top');
-const loaderBottomBtn = document.querySelector('.loader-bottom');
+const galleryBox = document.querySelector('.gallery-box');
+const loaderTop = document.querySelector('.loader-top');
+const loaderBottom = document.querySelector('.loader-bottom');
+const input = document.querySelector('input');
 const loadImg = document.querySelector('.load-image');
-
+const body = document.querySelector('body');
 
 let page = 1;
-let q = '';
+let q = 'cat';
 let per_page = 40;
 
 const lightbox = new SimpleLightbox('.gallery a', {
   nav: true,
+  captionDelay: 250,
   captionsData: 'alt',
   close: true,
   enableKeyboard: true,
@@ -28,7 +31,7 @@ loadImg.addEventListener('click', loadMore);
 
 async function onSubmit(event) {
   event.preventDefault();
-  loaderTopBtn.style.display = 'block';
+  loaderTop.style.display = 'block';
   loadImg.style.display = 'none';
   page = 1;
   q = event.target.elements.search.value.trim();
@@ -39,7 +42,7 @@ async function onSubmit(event) {
       position: 'topRight',
       message: 'Error enter any symbols',
     });
-    loaderTopBtn.style.display = 'none';
+    loaderTop.style.display = 'none';
     return;
   }
 
@@ -49,8 +52,8 @@ async function onSubmit(event) {
     } = await searchImg(q, page);
 
     if (hits.length > 0) {
-      loaderTopBtn.style.display = 'none';
-      gallery.innerHTML = renderImg(hits);;
+      loaderTop.style.display = 'none';
+      gallery.innerHTML = renderImg(hits);
       lightbox.refresh();
       iziToast.success({
         position: 'topRight',
@@ -70,9 +73,9 @@ async function onSubmit(event) {
       loadImg.style.display = 'none';
     }
   } catch (error) {
-    console.log('Error', error);
+    console.log('Error');
   } finally {
-    loaderTopBtn.style.display = 'none';
+    loaderTop.style.display = 'none';
     event.target.reset();
   }
 }
@@ -82,7 +85,7 @@ function searchImg(q, page) {
 
   return axios.get('/api/', {
     params: {
-      key: '41529899-34462864becb6db8698a10d1b',
+      key: '37773269-50f55f614e71cb99e92638715',
       q,
       image_type: 'photo',
       orientation: 'horizontal',
@@ -99,67 +102,51 @@ function renderImg(hits = []) {
       html +
       `<li class="gallery-item">
         <a href=${hit.largeImageURL}>
-          <img class="gallery-img" src=${hit.webformatURL} alt=${hit.tags} />
+          <img class="gallery-img" src =${hit.webformatURL} alt=${hit.tags}/>
         </a>
         <div class="gallery-text-box">
           <p>Likes: <span class="text-value">${hit.likes}</span></p>
           <p>views: <span class="text-value">${hit.views}</span></p>
           <p>comments: <span class="text-value">${hit.comments}</span></p>
           <p>downloads: <span class="text-value">${hit.downloads}</span></p>
-        </div>
+      </div>
       </li>`
     );
   }, '');
 }
 
 async function loadMore(event) {
-  loaderBottomBtn.style.display = 'block';
+  loaderBottom.style.display = 'block';
   loadImg.style.display = 'none';
   const listItem = document.querySelector('.gallery-item:first-child');
+  const itemHeight = listItem.getBoundingClientRect().height;
 
-  if (listItem) {
-    const itemHeight = listItem.getBoundingClientRect().height;
+  try {
+    page += 1;
 
-    try {
-      page += 1;
+    const {
+      data: { hits, totalHits },
+    } = await searchImg(q, page);
+    const totalPage = Math.ceil(totalHits / per_page);
 
-      const {
-        data: { hits, totalHits },
-      } = await searchImg(q, page);
-      const totalPage = Math.ceil(totalHits / per_page);
+    loadImg.style.display = 'block';
+    gallery.insertAdjacentHTML('beforeend', renderImg(hits));
+    lightbox.refresh();
 
-      loadImg.style.display = 'block';
-      gallery.insertAdjacentHTML('beforeend', renderImg(hits));
-      lightbox.refresh();
-
-      if (page === totalPage) {
-        loadImg.style.display = 'none';
-        return iziToast.info({
-          position: 'topRight',
-          message: `We're sorry, but you've reached the end of search results.`,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      loaderBottomBtn.style.display = 'none';
-      window.scrollBy({
-        top: 2 * itemHeight,
-        behavior: 'smooth',
+    if (page === totalPage) {
+      loadImg.style.display = 'none';
+      return iziToast.info({
+        position: 'topRight',
+        message: `We're sorry, but you've reached the end of search results.`,
       });
     }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loaderBottom.style.display = 'none';
+    window.scrollBy({
+      top: 2 * itemHeight,
+      behavior: 'smooth',
+    });
   }
 }
-
-// Додано прослуховування події прокрутки сторінки
-window.addEventListener('scroll', onScroll);
-const onScroll = () => {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-};
